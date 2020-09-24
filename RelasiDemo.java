1dafc4b47bdabda8bedfa0cfa44d6f9c20b7cd0d
Rizky Setiadi <69913956+MRSetiadi@users.noreply.github.com>
2020-09-24 10:29:16 +0700

    Create RelasiDemo.java

---
+++ RelasiDemo.java
@@ -0,0 +1,12 @@
+public class RelasiDemo{
+public static void main(String[] args) {
+//beberapa nilai
+int i = 37;
+int j = 42;
+int k = 42;
+System.out.println("Nilai variabel..."); 
+System.out.println("    i = " + i); 
+System.out.println("    j = " + j); 
+System.out.println("    k = " + k);
+}
+}

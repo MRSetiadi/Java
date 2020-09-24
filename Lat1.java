ccbbbe7853bec4579911d4a9b7e349f64d772616
Rizky Setiadi <69913956+MRSetiadi@users.noreply.github.com>
2020-09-24 10:28:03 +0700

    Create Lat1.java

---
+++ Lat1.java
@@ -0,0 +1,7 @@
+class Lat1 {
+  public static void main(String[] args) {
+    System.out.println("Selamat Belajar Java");
+    System.out.println("Semoga Sukses");
+    System.out.println("Created by Muhammad Rizky Setiadi");
+  }
+}
